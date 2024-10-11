dd1a5d0fdc6b84b7079cbf9ad0c66b4860745753
Game-Killer-s <artiom.bondar.1995@gmail.com>
2024-10-11 19:36:24 +0300

    Added object c2

--- Lesson_20/Source.cpp
+++ Lesson_20/Source.cpp
@@ -4,10 +4,17 @@
 using namespace std;
 
 int main() {
-	Car c1;
+	Car c1, c2;
 	cout << sizeof(c1) << endl;
 	c1.setModel("BMW");
 	c1.setMark("X5");
 	c1.setYear(2020);
+
+
+	c2.setModel("Renault");
+	c2.setMark("asdwae");
+	c2.setYear(2024);
+
 	c1.showCar();
+	c2.showCar();
 }
